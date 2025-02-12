f6dd526264cebf607ce7c004c798f234ad2538ee
Daniel_Cervantes <20690146@tecvalles.mx>
2025-02-12 17:58:46 -0600

    actulizacion de peticiones sea cada hora

--- src/componentes/Skill.jsx
+++ src/componentes/Skill.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { github } from './const'
-import './Skill.css';
+import React, { useState, useEffect } from "react";
+import { github } from "./const";
+import "./Skill.css";
+
 function Skill() {
+    const [statsUrl, setStatsUrl] = useState(github.stats + "&t=" + new Date().getTime());
+    const [mostUrl, setMostUrl] = useState(github.most + "&t=" + new Date().getTime());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setStatsUrl(github.stats + "&t=" + new Date().getTime());
+            setMostUrl(github.most + "&t=" + new Date().getTime());
+        }, 3600000); 
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div className="skill-container">
-            <img className='Github-stats'src={github.stats}/>
-            <img className='Most-languages'src={github.most}/>
+            <img className='Github-stats' src={statsUrl} alt="GitHub Stats" />
+            <img className='Most-languages' src={mostUrl} alt="Most Languages" />
         </div>
     );
 }
