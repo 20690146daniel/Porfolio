6439a1873fa7ca5881e9233e6bcc004e866c4e55
Daniel_Cervantes <20690146@tecvalles.mx>
2025-02-14 03:56:30 -0600

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,58 @@ import { Container, Grid, Paper } from '@mui/material';
 import Perfil from './componentes/perfil';
 import Skill from './componentes/Skill';
 import RecipeReviewCard from './componentes/RecipeReviewCard';
+import { blue } from '@mui/material/colors';
 
 function App() {
   return (
-    <Container>
-  <Grid container spacing={3}>
-    <Grid item xs={12} sm={3}>
-      <Paper style={{ padding: '10px' }}>
-        <Perfil />
-      </Paper>
-    </Grid>
-    <Grid item xs={12} sm={6}>
-      <Paper style={{ padding: '10px' }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <RecipeReviewCard />
-          </Grid>
+    <Container 
+      maxWidth="false" 
+      sx={{ 
+        backgroundColor: blue[500], 
+        height: '100vh', 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+      }}
+    >
+      <Grid container spacing={3}> 
+        <Grid item xs={4} flexGrow={1}> 
+          <Container maxWidth="sm"> 
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Paper style={{ padding: '10px' }}>
+                  <Perfil /> 
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
         </Grid>
-      </Paper>
-    </Grid>
-    <Grid item xs={12} sm={3}>
-      <Paper style={{ padding: '10px' }}>
-        <Skill />
-      </Paper>
-    </Grid>
-  </Grid>
-</Container>
 
+        <Grid item xs={4} flexGrow={1}> 
+          <Container maxWidth="md"> 
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Paper style={{ padding: '10px' }}>
+                  <RecipeReviewCard /> 
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grid>
+
+        <Grid item xs={4} flexGrow={1}> 
+          <Container maxWidth="md"> 
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <Paper style={{ padding: '10px' }}>
+                  <Skill /> 
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
