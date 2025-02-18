b66480643482826a2a3ae54b30d038f8802e61be
Daniel_Cervantes <20690146@tecvalles.mx>
2025-02-18 00:13:30 -0600

    cambios

---
+++ src/componentes/const.js
@@ -0,0 +1,32 @@
+
+export const user = {
+    name: "Daniel Cervantes Villanueva",
+    grado: "Ingeniero en Sistemas Computacionales",
+    img: "/images/cvimagen.jfif",
+    imageSize: "150px",
+    titulo: "Redes Sociales",
+    email: "danielcrv1100@gmail.com",
+    github: "https://github.com/20690146daniel/Daniel-Cervantes",
+    linkedin: "https://www.linkedin.com/in/daniel-cervantes-3783602b3/",
+
+};
+// export const github = {
+//     stats: "https://github-readme-stats.vercel.app/api?username=20690146daniel&hide_title=false&hide_rank=false&show_icons=true&include_all_commits=true&count_private=true&disable_animations=false&theme=dracula&locale=en&hide_border=false&order=1&cache_seconds=3600&t=" + new Date().getTime(),
+//     most: "https://github-readme-stats.vercel.app/api/top-langs?username=20690146daniel&locale=en&hide_title=false&layout=compact&card_width=320&langs_count=5&theme=dracula&hide_border=false&order=2&cache_seconds=3600&t=" + new Date().getTime()
+// };
+
+export const github = {
+    stats: "https://github-readme-stats.vercel.app/api?username=20690146daniel&hide_title=false&hide_rank=false&show_icons=true&include_all_commits=true&count_private=true&disable_animations=false&theme=light&locale=en&hide_border=false&order=1&cache_seconds=3600",
+    most: "https://github-readme-stats.vercel.app/api/top-langs?username=20690146daniel&locale=en&hide_title=false&layout=compact&card_width=320&langs_count=5&theme=light&hide_border=false&order=2&cache_seconds=3600",
+};
+
+export const skill ={
+flutter: "https://cdn.freelogovectors.net/wp-content/uploads/2023/09/flutter_logo-freelogovectors.net_.png",
+react:"https://pluspng.com/img-png/react-logo-png-img-react-logo-png-react-js-logo-png-transparent-png-1142x1027.png",
+laravel:"https://logospng.org/download/laravel/logo-laravel-icon-1024.png",
+node:"https://th.bing.com/th/id/R.38c3a911493bf29320caa2cb901e0ecb?rik=Pr5cKHu40c8q7w&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fnodejs-logo-png-node-js-on-dark-background-1843.png&ehk=8hzNCqqjl75a6u2B8slEau3ZMGb8GE1kG%2bW%2fzNsfNp8%3d&risl=&pid=ImgRaw&r=0",
+js:"https://www.citypng.com/public/uploads/preview/js-javascript-round-logo-icon-png-11662226392lsrrajcm0y.png",
+mongo:"https://pluspng.com/img-png/logo-mongodb-png-mongodb-logo-anything-but-the-simplest-of-web-applications-requires-a-database-to-store-and-serve-content-from-choosing-the-right-database-and-structuring-413.png",
+mysql:"https://www.pngplay.com/wp-content/uploads/7/Mysql-Logo-PNG-Background.png",
+php:"https://pngimg.com/uploads/php/php_PNG43.png"
+}
