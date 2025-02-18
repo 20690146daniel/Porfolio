dfb79bb52c774fe19aa64372c350d1f7468044c2
Daniel_Cervantes <20690146@tecvalles.mx>
2025-02-18 00:40:50 -0600

    update

--- src/componentes/layout.jsx
+++ src/componentes/layout.jsx
@@ -1,29 +1,37 @@
 import * as React from 'react';
-
 import RecipeReviewCard from './RecipeReviewCard';
 import Skill from './Skill';
-import Perfil from './perfil';
+import Perfil from './Perfil';
 import Tecnologias from './tecnologias.jsx';
 import './Layout.css';
 
 const Layout = () => {
+    const [isMobile, setIsMobile] = React.useState(window.innerWidth <= 600);
+
+    React.useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth <= 600);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
-
         <div className="layout">
-            <div className="horizontal-section ">
-                <Perfil />
-                <RecipeReviewCard />
-                <Tecnologias/>
-               
-            </div>
-            <Skill />
-
-
-
+            {isMobile ? (
+                <div className="vertical-section">
+                    <Perfil />
+                    <RecipeReviewCard />
+                    <Tecnologias />
+                    <Skill />
+                </div>
+            ) : (
+                <div className="horizontal-section">
+                    <Perfil />
+                    <RecipeReviewCard />
+                    <Tecnologias />
+                </div>
+            )}
         </div>
-
-
-    )
-}
+    );
+};
 
 export default Layout;
